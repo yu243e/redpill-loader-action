c29a17a33a939f19bb47362d621ff20f95025f06
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-01-30 21:54:22 +0800

    Update build-ds918-7.0.1.sh

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -29,7 +29,7 @@ cd redpill-load
 cp ${root}/user_config.DS918+.json ./user_config.json
 ./ext-manager.sh add https://raw.githubusercontent.com/RedPill-TTG/redpill-virtio/master/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/jumkey/redpill-load/develop/redpill-acpid/rpext-index.json
-./ext-manager.sh add https://raw.githubusercontent.com/dogodefi/mpt3sas/main/rpext-index.json
+./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/mpt3sas/rpext-index.json
 sudo ./build-loader.sh 'DS918+' '7.0.1-42218'
 mv images/redpill-DS918+_7.0.1-4221*.img ${root}/output/
 cd ${root}
