8eb43b926666f848210bd7f445bfab6c0d40674f
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-02-25 22:58:31 +0800

    Update build-fs6400-7.0.1.sh

--- build-fs6400-7.0.1.sh
+++ build-fs6400-7.0.1.sh
@@ -10,7 +10,7 @@ mkdir output
 cd FS6400-7.0.1
 
 # download redpill
-git clone -b test --depth=1 https://github.com/dogodefi/redpill-lkm.git
+git clone -b test1 --depth=1 https://github.com/dogodefi/redpill-lkm.git
 git clone -b test --depth=1 https://github.com/dogodefi/redpill-load.git
 
 # download syno toolkit
