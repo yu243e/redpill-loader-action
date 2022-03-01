837ab1baa3af1f342bf37f510d45736afa302535
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-03-01 14:30:00 +0800

    Update build-ds3622xsp-7.1.sh

--- build-ds3622xsp-7.1.sh
+++ build-ds3622xsp-7.1.sh
@@ -26,12 +26,12 @@ output=$(xz -dc < rd.gz 2>/dev/null | cpio -idm 2>&1)
 mkdir extract && cd extract
 cp ../usr/lib/libcurl.so.4 ../usr/lib/libmbedcrypto.so.5 ../usr/lib/libmbedtls.so.13 ../usr/lib/libmbedx509.so.1 ../usr/lib/libmsgpackc.so.2 ../usr/lib/libsodium.so ../usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 ../usr/syno/bin/scemd ./
 ln -s scemd syno_extract_system_patch
-ln -lh
+ls -lh
 curl --location https://global.download.synology.com/download/DSM/beta/7.1/42550/DSM_DS3622xs%2B_42550.pat --output 42250.pat
 mkdir output-pat
 LD_LIBRARY_PATH=. ./syno_extract_system_patch 42250.pat output-pat
 cd output-pat && tar -zcvf 42250.pat *
-ls 42250.pat
+ls -l 42250.pat
 cp 42250.pat ${root}/${workpath}/redpill-load/cache/ds3622xsp_42250.pat
 cd ../../../
 
