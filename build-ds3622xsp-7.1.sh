10819fe8b41799d2cbb91a89c7106437929b0d39
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-03-01 15:07:58 +0800

    Update build-ds3622xsp-7.1.sh

--- build-ds3622xsp-7.1.sh
+++ build-ds3622xsp-7.1.sh
@@ -17,7 +17,7 @@ cd $workpath
 
 #download old pat for syno_extract_system_patch # thanks for jumkey's idea.
 mkdir synoesp
-curl --location https://cndl.synology.cn/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
+curl --location https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
 tar -C./synoesp/ -xf oldpat.tar.gz rd.gz
 cd synoesp
 
