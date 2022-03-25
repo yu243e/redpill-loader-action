af8f8fe1b7dcc5bd506858c9306d0998d3822bd4
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-03-26 01:27:48 +0800

    Update build-universal-7.1.sh

--- build-universal-7.1.sh
+++ build-universal-7.1.sh
@@ -41,7 +41,7 @@ esac
 sudo apt-get update && sudo apt-get install --yes --no-install-recommends ca-certificates build-essential git libssl-dev curl cpio bspatch vim gettext bc bison flex dosfstools kmod jq
 root=`pwd`
 os_version=$2
-pat_address="https://global.download.synology.com/download/DSM/release/7.1/"${os_version}"DSM_"${dsmodel}"_"${os_version}".pat"
+pat_address="https://global.download.synology.com/download/DSM/release/7.1/"${os_version}"/DSM_"${dsmodel}"_"${os_version}".pat"
 echo ${pat_address}
 #https://global.download.synology.com/download/DSM/release/7.1/42621/DSM_DS3622xs%2B_42621.pat
 
