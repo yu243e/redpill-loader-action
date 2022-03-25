c3d7fcd05b957101f8f73f03a7b735154f231484
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-03-25 13:47:46 +0800

    Update build-ds3622xsp-7.1.sh

--- build-ds3622xsp-7.1.sh
+++ build-ds3622xsp-7.1.sh
@@ -45,7 +45,7 @@ mkdir extract && cd extract
 cp ../usr/lib/libcurl.so.4 ../usr/lib/libmbedcrypto.so.5 ../usr/lib/libmbedtls.so.13 ../usr/lib/libmbedx509.so.1 ../usr/lib/libmsgpackc.so.2 ../usr/lib/libsodium.so ../usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 ../usr/syno/bin/scemd ./
 ln -s scemd syno_extract_system_patch
 
-curl --location  pat-address --output ${os_version}.pat
+curl --location  ${pat-address} --output ${os_version}.pat
 
 sudo LD_LIBRARY_PATH=. ./syno_extract_system_patch ${os_version}.pat output-pat
 
