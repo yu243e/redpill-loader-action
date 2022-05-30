23c05e8102f914b16fd3a23d3acde877c35a52c7
dogodefi <82947946+dogodefi@users.noreply.github.com>
2022-05-30 17:49:37 +0800

    Update build-fs6400-7.0.1.sh

--- build-fs6400-7.0.1.sh
+++ build-fs6400-7.0.1.sh
@@ -10,7 +10,7 @@ mkdir output
 cd FS6400-7.0.1
 
 # download redpill
-git clone -b test1 --depth=1 https://github.com/dogodefi/redpill-lkm.git
+git clone -b develop --depth=1 https://github.com/jumkey/redpill-lkm.git
 git clone -b test --depth=1 https://github.com/dogodefi/redpill-load.git
 
 # download syno toolkit
@@ -29,7 +29,7 @@ cd ..
 # build redpill-load
 cd redpill-load
 cp -f ${root}/user_config.FS6400.json ./user_config.json
-#./ext-manager.sh add https://raw.githubusercontent.com/dogodefi/mpt3sas/offical/rpext-index.json
+./ext-manager.sh add https://raw.githubusercontent.com/dogodefi/mpt3sas/offical/rpext-index.json
 sudo ./build-loader.sh 'FS6400' '7.0.1-42218'
 mv images/redpill-FS6400_7.0.1-4221*.img ${root}/output/
 cd ${root}
